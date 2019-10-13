d6838e240b3383ebc6f8f3b506c5f08dde209cdd
hamoghamdi <haneen.aljabrah@gmail.com>
2019-10-14 00:04:09 +0300

    feat: create store, add Provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+import reducer from "./reducers"
+import middleware from "./middleware"
+
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
