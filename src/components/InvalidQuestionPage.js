e7df4ab2e1ec976faf2943ecbc428525e0f2e9ea
hamoghamdi <haneen.aljabrah@gmail.com>
2019-10-28 22:14:37 +0300

    fix: re-add InvalidQuestionPage.js

---
+++ src/components/InvalidQuestionPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InvalidQuestionPage = () => {
+  return (
+    <div>
+      <h2>404 Not Found</h2>
+      <h3>This question cannot be found</h3>
+    </div>
+  );
+};
+
+export default InvalidQuestionPage;
